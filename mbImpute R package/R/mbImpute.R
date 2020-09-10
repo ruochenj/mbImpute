17678b3b1cef6e0ff775dd33ee971008c76e5756
ruochenj <ruochenj@gmail.com>
2020-09-09 23:43:33 -0700

    mbImpute V2

--- mbImpute R package/R/mbImpute.R
+++ mbImpute R package/R/mbImpute.R
@@ -9,27 +9,21 @@
 #' should be excluded from the meta data.
 #' @param D A matrix of dimension m * m corresponding to the distance matrix of taxa generated from an edge set. If an edge set is provided, you can use distance_mat function in our
 #' package to generate the distance matrix D.
-#' @param psi A scalar corresponding to the importance weight of the taxa distance matrix. This arguement is only useful if cross_valid is FALSE.
 #' @param k A scalar corresponding to the number of nearest taxa in a phylogenetic tree we will use to impute a missing value. Theoretically, the larger k, the more accurate our imputation
 #' will be (required that k <= m).
-#' @param cross_valid A boolean indicating whether we will use cross validation to find the best psi. If cross_valid is TRUE, we use grid search and find the best psi in the set 0.5, 1, 1.5,
-#' 2.
 #' @param parallel A boolean indicating whether to use parallel or not. Default is FALSE.
 #' @param ncores A scalar corresponding to the number of cores to use. It is only used when parallel = TRUE. Default is 1.
-#' @param type_1 A boolean indicating which type of mbImpute to use. Details can be found in the Vignette.
+#' @param unnormalized A boolean indicating whether the data has been normalized. Details can be found in the Vignette.
 #' @return A list three imputed OTU matrices.
 #' imp_count_mat_lognorm: imputed normalized and log transformed matrix.
 #' imp_count_mat_norm : imputed normalized count matrix with library size of each sample equal to 10^6.
 #' imp_count_mat_origlibsize: imputed countmatrix at the original library size.
 #' We recommend to use the first one imp_count_mat_lognorm. So that the log scaled counts follows approximately Normal for each taxon across samples.
 #' @export
-mbImpute <- function(condition = NULL, otu_tab = NULL, meta_data = NULL, D = NULL, psi = 2, k = 5, cross_valid = FALSE, parallel = FALSE, ncores = 1, type_1 = T){
+mbImpute <- function(condition = NULL, otu_tab = NULL, meta_data = NULL, D = NULL, k = 5, parallel = FALSE, ncores = 1, unnormalized = T){
   #separate the samples of the dataset into different conditions
   #process the otu_table
   set.seed(1)
-  scale <- rowSums(otu_tab) / (10^6)
-  otu_tab <- otu_tab / rowSums(otu_tab) * 10^6
-  otu_tab <- log10(otu_tab + 1.01)
   if(is.null(otu_tab)){
     print("No OTU table as input")
     return(NULL)
@@ -65,7 +59,11 @@ mbImpute <- function(condition = NULL, otu_tab = NULL, meta_data = NULL, D = NUL
     D = matrix(2, ncol = dim(otu_tab)[2], nrow = dim(otu_tab)[2])
     diag(D) <- 1
   }
-
+  if(unnormalized){
+    scale <- rowSums(otu_tab) / (10^6)
+    otu_tab <- otu_tab / rowSums(otu_tab) * 10^6
+    otu_tab <- log10(otu_tab + 1.01)
+  }
   cond_set <- unique(condition)
   for(cond in cond_set){
     idx <- which(condition %in% cond)
@@ -76,39 +74,7 @@ mbImpute <- function(condition = NULL, otu_tab = NULL, meta_data = NULL, D = NUL
     }else{
       meta_data_cond <- meta_data[idx,]
     }
-    if(!parallel){
-      if(cross_valid){
-        psi_vec = c(0.5, 1, 1.5, 2)
-        min_mse = 1000
-        for(psi in psi_vec){
-          impute_val <- data_fit2(otu_tab_cond, meta_data_cond, D, psi, k = k, type_1 = type_1)
-          if(impute_val$mse < min_mse){
-            otu_tab[idx,] = impute_val$y_imp
-            min_mse = impute_val$mse
-          }
-        }
-      }else{
-        impute_val <- data_fit2(otu_tab_cond, meta_data_cond, D, psi, k = k, type_1 = type_1)
-        otu_tab[idx,] = impute_val$y_imp
-        #rm(impute_val)
-      }
-    }else{
-      if(cross_valid){
-        psi_vec = c(0.5, 1, 1.5, 2)
-        min_mse = 1000
-        for(psi in psi_vec){
-          impute_val <- data_fit2(otu_tab_cond, meta_data_cond, D, psi, k = k, parallel = parallel, ncores = ncores, type_1 = type_1)
-          if(impute_val$mse < min_mse){
-            otu_tab[idx,] = impute_val$y_imp
-            min_mse = impute_val$mse
-          }
-        }
-      }else{
-        impute_val <- data_fit2(otu_tab_cond, meta_data_cond, D, psi, k = k, parallel = parallel, ncores = ncores, type_1 = type_1)
-        otu_tab[idx,] = impute_val$y_imp
-        #rm(impute_val)
-      }
-    }
+    otu_tab[idx,] <- data_fit2(otu_tab_cond, meta_data_cond, D, k = k, unnormalized = unnormalized)
   }
   print("Finished.")
   imp_count_mat_norm <- floor(10^(otu_tab) - 1.01)
