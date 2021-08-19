5bb21f49307774165b3d1263e6523b0c700ee36a
ruochenj <ruochenj@gmail.com>
2021-08-18 22:27:30 -0700

    Add files via upload

---
+++ mbImpute R package/R/edges.R
@@ -0,0 +1,10 @@
+#' @title edges of phylogenetic tree of Karlsson et al.
+#'
+#' @description The edges for 344 taxa
+#'
+#' @format A data frame containing 145 rows and 12 columns, the first column indicates the condition we are interested in.
+#' \describe{
+#' \item{x}{1}
+#' \item{y}{2}
+#' }
+"edges"
---
+++ mbImpute R package/R/otu_tab.R
@@ -0,0 +1,10 @@
+#' @title OTU table for Karlsson et al.
+#'
+#' @description The OTU table inidcating the abundances of 344 taxa in 145 subjects (samples).
+#'
+#' @format A data frame containing 145 rows and 12 columns, the first column indicates the condition we are interested in.
+#' \describe{
+#' \item{x}{1}
+#' \item{y}{2}
+#' }
+"otu_tab"
